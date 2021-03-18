07d2bc022ec328191d981325d1d45aa9c83d5aa6
cour10eygrace <courtneygrace125@gmail.com>
2021-03-18 11:27:06 -0600

    updated test R script

--- test.R
+++ test.R
@@ -1,2 +1,3 @@
-#this is an R script that we will make changes to during the Github workshop for WISE
-#THIS IS SO FUN!!!
+#R script that we will make changes to during the Github workshop for WISE
+#THIS IS SO FUN!!!!!
+#This is a new line. 
