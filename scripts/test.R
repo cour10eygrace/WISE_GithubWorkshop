5ce486068f256a10e555741063f662e921edf949
cour10eygrace <courtneygrace125@gmail.com>
2021-03-18 12:41:01 -0600

    adding more to my favorite line

--- scripts/test.R
+++ scripts/test.R
@@ -1,5 +1,5 @@
 #R script that we will make changes to during the Github workshop for WISE
 #THIS IS SO FUN!!!!!
-#This is a new line. 
+#This is a new line. I love this line I'm going to keep adding more....
 
 dat<-read.csv("data/data.csv")
